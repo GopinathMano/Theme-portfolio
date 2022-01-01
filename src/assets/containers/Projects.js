3ce12781a0c03534bb3315e9ab6a5dbefd724289
Gopinath Mano <gopimano4242@gmail.com>
2022-01-01 13:58:57 +0530

    code edited on 01/01/2020

--- src/assets/containers/Projects.js
+++ src/assets/containers/Projects.js
@@ -4,6 +4,9 @@ import { CardColumns, Card, Button } from "react-bootstrap";
 import amazon from "../images/react-shopy.png";
 import recipe from "../images/My-Recipies.png";
 import blog from "../images/motorDairies.png";
+import quizApp from "../images/quizApp.png";
+import mentorApp from "../images/mentor-manger.png";
+import simonGame from "../images/simonGame.png";
 
 const Projects = ({ theme }) => {
   const newTheme = `${theme} d-flex flex-column min-vh-100 justify-content-center align-items-center text-center`;
@@ -30,8 +33,22 @@ const Projects = ({ theme }) => {
             <Card.Img variant="top" src={amazon} style={styles.cardImage} />
             <Card.Body>
               <Card.Title>Amazon Clone</Card.Title>
-              <Card.Link href="https://lucid-carson-b3fd8a.netlify.app/">
-                <Button variant="primary">Live-Website</Button>
+              <Card.Text>
+                Technology used are <br></br>
+                <strong>ReactJs -NodeJs -MongoDB -ExpressJs</strong>
+              </Card.Text>
+
+              <Card.Link
+                target="_blank"
+                href="https://github.com/GopinathMano/React-Shopy-Frontend"
+              >
+                <Button variant="dark">Source-code</Button>
+              </Card.Link>
+              <Card.Link
+                target="_blank"
+                href="https://lucid-carson-b3fd8a.netlify.app/"
+              >
+                <Button variant="light">Live-Website</Button>
               </Card.Link>
             </Card.Body>
           </Card>
@@ -39,8 +56,21 @@ const Projects = ({ theme }) => {
             <Card.Img variant="top" src={recipe} style={styles.cardImage} />
             <Card.Body>
               <Card.Title>MY RECIPIES APP</Card.Title>
-              <Card.Link href="https://myrecipes02.netlify.app/">
-                <Button variant="primary">Live-Website</Button>
+              <Card.Text>
+                Technology used are <br></br>
+                <strong>ReactJs -NodeJs -MongoDB -ExpressJs</strong>
+              </Card.Text>
+              <Card.Link
+                target="_blank"
+                href="https://github.com/GopinathMano/React-MyRecipeApp-Frontend"
+              >
+                <Button variant="dark">Source-code</Button>
+              </Card.Link>
+              <Card.Link
+                target="_blank"
+                href="https://myrecipes02.netlify.app/"
+              >
+                <Button variant="light">Live-Website</Button>
               </Card.Link>
             </Card.Body>
           </Card>
@@ -48,8 +78,90 @@ const Projects = ({ theme }) => {
             <Card.Img variant="top" src={blog} style={styles.cardImage} />
             <Card.Body>
               <Card.Title>MOTORCYCLE-DIARIES</Card.Title>
-              <Card.Link href="https://epic-bassi-425708.netlify.app/">
-                <Button variant="primary">Live-Website</Button>
+              <Card.Text>
+                Technology used are <br></br>
+                <strong>ReactJs -NodeJs -MongoDB -ExpressJs</strong>
+              </Card.Text>
+              <Card.Link
+                target="_blank"
+                href="https://github.com/GopinathMano/mern-BikerDiary-frontend"
+              >
+                <Button variant="dark">Source-code</Button>
+              </Card.Link>
+              <Card.Link
+                target="_blank"
+                href="https://epic-bassi-425708.netlify.app/"
+              >
+                <Button variant="light">Live-Website</Button>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </CardColumns>
+        <CardColumns>
+          <Card>
+            <Card.Img variant="top" src={mentorApp} style={styles.cardImage} />
+            <Card.Body>
+              <Card.Title>Mentor Manager</Card.Title>
+              <Card.Text>
+                Technology used are <br></br>
+                <strong>ReactJs -NodeJs -MongoDB -ExpressJs</strong>
+              </Card.Text>
+              <Card.Link
+                target="_blank"
+                href="https://github.com/GopinathMano/mentorAssign-frontend"
+              >
+                <Button variant="dark">Source-code</Button>
+              </Card.Link>
+              <Card.Link
+                target="_blank"
+                href="https://muthu-assign-mentor.netlify.app/"
+              >
+                <Button variant="light">Live-Website</Button>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Img variant="top" src={quizApp} style={styles.cardImage} />
+            <Card.Body>
+              <Card.Title>React Quiz App</Card.Title>
+              <Card.Text>
+                Technology used are <br></br>
+                <strong>ReactJs </strong>
+              </Card.Text>
+              <Card.Link
+                target="_blank"
+                href="https://github.com/GopinathMano/react-quiz-app"
+              >
+                <Button variant="dark">Source-code</Button>
+              </Card.Link>
+              <Card.Link
+                target="_blank"
+                href="https://millaniorquiz.netlify.app/"
+              >
+                <Button variant="light">Live-Website</Button>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={simonGame} style={styles.cardImage} />
+            <Card.Body>
+              <Card.Title>Simon Game</Card.Title>
+              <Card.Text>
+                Technology used are <br></br>
+                <strong>ReactJs </strong>
+              </Card.Text>
+              <Card.Link
+                target="_blank"
+                href="https://github.com/GopinathMano/Color-Memory-game"
+              >
+                <Button variant="dark">Source-code</Button>
+              </Card.Link>
+              <Card.Link
+                target="_blank"
+                href="https://elastic-beaver-5d9f85.netlify.app/"
+              >
+                <Button variant="light">Live-Website</Button>
               </Card.Link>
             </Card.Body>
           </Card>
